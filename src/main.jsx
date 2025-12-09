e4ed38ebed13ad34c1be384a9da6cd339369582e
PracticumTutor <PracticumTutor@gmail.com>
2025-12-08 21:10:34 -0500

    Deploment 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
+const basename =
+  import.meta.env.MODE === "production" ? "/se_project_news_explorer" : "/";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
